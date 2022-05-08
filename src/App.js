5548f329e2ff7c379fed42b373e2cb6983aa78d0
Rohan Shinde <84450515+rohanshinde007@users.noreply.github.com>
2022-05-08 23:22:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import About from './components/About';
+
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import './App.css';
